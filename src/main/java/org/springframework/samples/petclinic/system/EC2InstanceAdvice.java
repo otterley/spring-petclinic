5d9c3c8dec94dca6a93484e92c3add6314dcf7b8
Michael Fischer <fiscmi@amazon.com>
2023-02-27 23:02:51 -0800

    WIP

--- src/main/java/org/springframework/samples/petclinic/system/EC2InstanceAdvice.java
+++ src/main/java/org/springframework/samples/petclinic/system/EC2InstanceAdvice.java
@@ -1,6 +1,5 @@
 package org.springframework.samples.petclinic.system;
 
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -10,16 +9,17 @@ import software.amazon.awssdk.imds.Ec2MetadataResponse;
 @ControllerAdvice
 public class EC2InstanceAdvice {
 
-    private static Ec2MetadataClient ec2MetadataClient = Ec2MetadataClient.create();
-    private static String instanceType;
+	private static Ec2MetadataClient ec2MetadataClient = Ec2MetadataClient.create();
+
+	private static String instanceType;
 
 	@ModelAttribute("ec2InstanceType")
 	public String getEC2InstanceType() {
-        if (instanceType == null) {
-            Ec2MetadataResponse ec2MetadataResponse = ec2MetadataClient.get("/latest/meta-data/instance-type");
-            instanceType = ec2MetadataResponse.asString();
-        }
-        return instanceType;
+		if (instanceType == null) {
+			Ec2MetadataResponse ec2MetadataResponse = ec2MetadataClient.get("/latest/meta-data/instance-type");
+			instanceType = ec2MetadataResponse.asString();
+		}
+		return instanceType;
 	}
 
 	@ModelAttribute("isGravitonInstance")

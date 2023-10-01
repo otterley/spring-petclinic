7a3a6a5f4b80f883864872a9c58db56e32ad4d53
Michael Fischer <fiscmi@amazon.com>
2023-10-01 14:15:06 -0700

    Add debugging

--- codedeploy/scripts/start.sh
+++ codedeploy/scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -evx -o pipefail
+
 systemctl enable spring-pet-clinic.service
 systemctl start spring-pet-clinic.service
--- codedeploy/scripts/stop.sh
+++ codedeploy/scripts/stop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -evx -o pipefail
+
 if systemctl is-active --quiet spring-pet-clinic.service; then
   systemctl stop spring-pet-clinic.service
 fi
